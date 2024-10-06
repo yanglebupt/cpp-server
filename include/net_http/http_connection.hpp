e3057405dd0f628b096557fc1f2814f9242e01ec
杨乐 <2735686935@qq.com>
2024-10-06 14:07:28 +0800

    keep alive 功能添加

--- include/net_http/http_connection.hpp
+++ include/net_http/http_connection.hpp
@@ -1,72 +1,87 @@
 #pragma once
 
 #include "http_typedef.hpp"
-#include "../utils/functions.hpp"
+#include "../utils/owned_message_interface.hpp"
+#include "../utils/tsqueue.hpp"
 #include <iostream>
-#include <thread>
-
-int i = 0;
+#include <memory>
 
 namespace net::nhttp
 {
   class http_connection : public std::enable_shared_from_this<http_connection>
   {
-
   public:
-    http_connection(tcp::socket socket, uint8_t keep_alive_timeout) : socket(std::move(socket)), keep_alive_timeout(keep_alive_timeout) {}
+    http_connection(tcp::socket socket, uint8_t keep_alive_timeout, uint8_t max_connection_count, tsqueue<owned_message_interface<Request, http_connection>> &qIn) : socket(std::move(socket)),
+                                                                                                                                                                     keep_alive_timeout(keep_alive_timeout), max_connection_count(max_connection_count), qIn(qIn) {}
 
     ~http_connection()
     {
-      std::cout << req.method_string() << " " << req.Get<std::string>("path") << " Connection Destroyed!" << std::endl;
+      std::cout << remote_endpoint() << " Connection Destroyed!" << std::endl;
     }
 
-    void ReadRequest(RequestHandlerCallback _Callback)
+    void ConnectToClient()
     {
-      http::async_read(socket, buffer, req, [self = shared_from_this(), _Callback](std::error_code ec, size_t length)
-                       { if(!ec){ self->HandleRequest(_Callback); } });
-
-      // 短连接时间到了，关闭 socket
+      ReadRequest();
+      // 短连接时间到了，不能立刻关闭 socket，需要等待接收和发送完毕后才关闭
       deadline.async_wait([self = shared_from_this()](std::error_code ec)
-                          { std::cout << self->req.method_string() << " " << self->req.Get<std::string>("path") << " Timeout: " << ec << std::endl;
-                            if(!ec) self->socket.close(); });
+                          { 
+                            std::cout << self->remote_endpoint() << " Connection Timeout!" << std::endl; 
+                            self->timeout = true; });
     }
 
-    void HandleRequest(RequestHandlerCallback _Callback)
+    void ReadRequest()
     {
-      // 设置响应头
-      res.version(req.version());
-      res.keep_alive(false);
-
-      // 解析地址，得到 query，需要为 req 添加新属性
-      auto [url, search] = SplitURLSearch(req.target());
-      req.Add("path", url);
-      req.Add("search", search);
-      req.Add("query", URLSearchParams(search));
-
-      _Callback(req, res);
-
-      // 写入响应
-      res.set(http::field::server, "Beast");
-      res.content_length(res.body().size());
-      http::async_write(socket, res, [self = shared_from_this()](std::error_code ec, size_t length)
-                        {
-                          self->socket.shutdown(tcp::socket::shutdown_send);
-                          // 定时器不要再等待了，cancel 会以 asio::error::operation_aborted 触发回调
-                          self->deadline.cancel(); });
+      // 接收数据的 buffer
+      std::shared_ptr<beast::flat_buffer> buffer = std::make_shared<beast::flat_buffer>(1024 * 8);
+      std::shared_ptr<Request> req = std::make_shared<Request>();
+      http::async_read(socket, *buffer, *req, [self = shared_from_this(), req, buffer](std::error_code ec, size_t length)
+                       { if(!ec){
+                          self->c_count++;
+                          bool need_keep_alive = self->NeedKeepAlive(*req);
+                          owned_message_interface<Request, http_connection> msg;
+                          msg.remote = self->shared_from_this();
+                          msg.msg = std::move(*req);
+                          self->qIn.emplace_back(msg);
+                          if (need_keep_alive)
+                            self->ReadRequest();
+                        } });
     }
+
+    void WriteResponse(std::shared_ptr<Response> res, bool keep_alive)
+    {
+      // 写入响应
+      res->set(http::field::server, "Beast");
+      res->content_length(res->body().size());
+      res->keep_alive(keep_alive);
+      if (keep_alive)
+        res->set(http::field::keep_alive, "timeout=" + std::to_string(keep_alive_timeout) + ", max=" + std::to_string(max_connection_count));
+
+      http::async_write(socket, *res, [self = shared_from_this(), res, keep_alive](std::error_code ec, size_t length)
+                        {
+                          if(!ec){
+                            if (!keep_alive){
+                              self->socket.shutdown(tcp::socket::shutdown_send);
+                              // 定时器不要再等待了，cancel 会以 asio::error::operation_aborted 触发回调
+                              if (!self->timeout)
+                                self->deadline.cancel();
+                            }
+                          } });
+    }
+
+    bool NeedClose() { return timeout || c_count >= max_connection_count; }
+
+    bool NeedKeepAlive(Request &req) { return NeedClose() ? false : (req.keep_alive() || (req.find(http::field::connection) != req.end() && req.at(http::field::connection) == "keep-alive")); }
+
+    tcp::endpoint remote_endpoint() { return socket.remote_endpoint(); }
 
   protected:
     uint8_t keep_alive_timeout;
-
+    uint8_t max_connection_count;
+    uint8_t c_count = 0;
+    bool timeout = false;
     tcp::socket socket;
-    // 接收数据的 buffer
-    beast::flat_buffer buffer{1024 * 8};
-
-    // 后面会将数据 buffer 解析到 req/res 中
-    Request req;
-    Response res;
-
     // 定时器，超时检测，http 默认是短连接，超时直接断开连接
     asio::steady_timer deadline{socket.get_executor(), std::chrono::seconds(keep_alive_timeout)};
+    tsqueue<owned_message_interface<Request, http_connection>> &qIn;
   };
 }
--- include/net_http/http_server.hpp
+++ include/net_http/http_server.hpp
@@ -2,18 +2,21 @@
 
 #include "http_router.hpp"
 #include "http_connection.hpp"
+#include "../utils/functions.hpp"
+#include "../utils/logic_systerm.hpp"
 #include <algorithm>
 #include <iostream>
 
 namespace net::nhttp
 {
   // server 本身也是一个路由
-  class http_server : public http_router
+  class http_server : public http_router, public logic_system<Request, http_connection>
   {
   public:
     // 一些控制量
     std::vector<verb> NO_ROUTE_METHODS{verb::options};
     uint8_t keep_alive_timeout = 60;
+    uint8_t max_connection_count = 5;
 
     http_server(uint16_t port) : m_acceptor(ctx, tcp::endpoint(tcp::v4(), port)) {}
 
@@ -32,6 +35,8 @@ namespace net::nhttp
         asio::signal_set exit_signals(ctx, SIGINT, SIGTERM);
         exit_signals.async_wait([this](auto, auto)
                                 { ctx.stop(); });
+        // 开启消息子线程
+        this->StartHandleMessages();
 
         // 一直循环监听请求
         WaitForClientConnection();
@@ -56,25 +61,9 @@ namespace net::nhttp
         {
           std::cout << "[SERVER] New Connection: " << socket.remote_endpoint() << std::endl;
           std::shared_ptr<http_connection>
-              client = std::make_shared<http_connection>(std::move(socket), keep_alive_timeout);
-          // 回调函数结果 client 计数减一，不会释放，因为里面的回调函数，都会 shared_from_this() 来计数加一，延长生命周期
-          client->ReadRequest([this](Request &req, Response &res)
-                              { 
-                                const std::string &method = req.method_string();
-                                const std::string &target = req.Get<std::string>("path");
-                                std::cout << method << " " << target << std::endl;
-                                bool has_route = false;
-                                bool need_route = std::find(NO_ROUTE_METHODS.begin(), NO_ROUTE_METHODS.end(), req.method()) == NO_ROUTE_METHODS.end();
-                                const std::deque<http_middleware_func> &middlewares = this->get_matched_paths(target, method, has_route);
-                                if (!need_route || (need_route && has_route))
-                                  this->ApplyMiddleware(middlewares, req, res, 0, middlewares.size());
-                                else
-                                {
-                                  // 如果整个过程没有 route 中间件，则返回 404
-                                  res.result(http::status::not_found);
-                                  res.set(http::field::content_type, "text/plain");
-                                  beast::ostream(res.body()) << "[" << method << " " << target << "] is not found!";
-                                } });
+              client = std::make_shared<http_connection>(std::move(socket), keep_alive_timeout, max_connection_count, this->InComing());
+          // 回调函数结果 client 计数加一，不会释放，因为里面的回调函数，都会 shared_from_this() 来计数加一，延长生命周期
+          client->ConnectToClient();
         }
         WaitForClientConnection(); });
     }
@@ -88,6 +77,37 @@ namespace net::nhttp
       md(req, res, [&]()
          { ApplyMiddleware(middlewares, req, res, cur + 1, total); });
     }
+
+    // 接收到客户端消息包
+    void OnMessage(std::shared_ptr<http_connection> client, Request &req) override
+    {
+      std::shared_ptr<Response> res = std::make_shared<Response>();
+      bool need_keep_alive = client->NeedKeepAlive(req);
+      res->version(req.version());
+
+      // 解析地址，得到 query，需要为 req 添加新属性
+      auto [url, search] = SplitURLSearch(req.target());
+      req.Add("path", url);
+      req.Add("search", search);
+      req.Add("query", URLSearchParams(search));
+
+      const std::string &method = req.method_string();
+      const std::string &target = req.Get<std::string>("path");
+      std::cout << client->remote_endpoint() << " " << method << " " << target << std::endl;
+      bool has_route = false;
+      bool need_route = std::find(NO_ROUTE_METHODS.begin(), NO_ROUTE_METHODS.end(), req.method()) == NO_ROUTE_METHODS.end();
+      const std::deque<http_middleware_func> &middlewares = this->get_matched_paths(target, method, has_route);
+      if (!need_route || (need_route && has_route))
+        ApplyMiddleware(middlewares, req, *res, 0, middlewares.size());
+      else
+      {
+        // 如果整个过程没有 route 中间件，则返回 404
+        res->result(http::status::not_found);
+        res->set(http::field::content_type, "text/plain");
+        beast::ostream(res->body()) << "[" << method << " " << target << "] is not found!";
+      }
+      client->WriteResponse(res, need_keep_alive);
+    };
 
   private:
     asio::io_context ctx;
